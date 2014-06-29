75f9fb3b9f3c4e4ad2aaff9486f5dccb7e420b86
Johannes Schickling <schickling.j@gmail.com>
2014-06-29 22:02:01 +0200

    setup

--- setup-minimal.sh
+++ setup-minimal.sh
@@ -1,18 +1,14 @@
 #! /bin/bash
 
-# setup dotfiles
+# clone dotfiles
 git clone https://github.com/schickling/dotfiles.git ~/.dotfiles
 
-# set env
+# link vim
+ln -s ~/.dotfiles/vim/bundle ~/.vim/bundle
+ln -s ~/.dotfiles/vim/colors ~/.vim/colors
+ln -s ~/.dotfiles/vimrc ~/.vimrc
+
+# setup env
 echo "export DOTFILES=$HOME/.dotfiles" >> ~/.bashrc
 echo "source $DOTFILES/aliases/unix.sh" >> ~/.bashrc
 echo "source $DOTFILES/aliases/git.sh" >> ~/.bashrc
-source ~/.bashrc
-
-# setup vim
-wget -P ~/.vim/colors https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim
-ln -s $DOTFILES/vimrc ~/.vimrc
-
-# setup vundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
