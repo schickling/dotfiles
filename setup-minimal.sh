7745a8c95573bbfe73da7e4564c1b5dea4a07668
Johannes Schickling <schickling.j@gmail.com>
2014-06-29 22:51:08 +0200

    dot

--- setup-minimal.sh
+++ setup-minimal.sh
@@ -9,9 +9,9 @@ ln -s ~/.dotfiles/vim/colors ~/.vim/colors
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 
 # setup env
+echo "source .dotfiles/aliases/unix.sh" >> ~/.bashrc
+echo "source .dotfiles/aliases/git.sh" >> ~/.bashrc
 echo "export DOTFILES=$HOME/.dotfiles" >> ~/.bashrc
-echo 'source $DOTFILES/aliases/unix.sh' >> ~/.bashrc
-echo 'source $DOTFILES/aliases/git.sh' >> ~/.bashrc
 
 # setup vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
