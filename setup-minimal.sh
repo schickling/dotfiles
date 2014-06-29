8a30225a51780a7e22d7eb274b9bdc7bcfa44f5f
Johannes Schickling <schickling.j@gmail.com>
2014-06-29 22:36:06 +0200

    r

--- setup-minimal.sh
+++ setup-minimal.sh
@@ -5,7 +5,6 @@ git clone --recursive https://github.com/schickling/dotfiles.git ~/.dotfiles
 
 # link vim
 mkdir ~/.vim
-ln -s ~/.dotfiles/vim/bundle ~/.vim/bundle
 ln -s ~/.dotfiles/vim/colors ~/.vim/colors
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 
@@ -13,3 +12,7 @@ ln -s ~/.dotfiles/vimrc ~/.vimrc
 echo "export DOTFILES=$HOME/.dotfiles" >> ~/.bashrc
 echo "source $DOTFILES/aliases/unix.sh" >> ~/.bashrc
 echo "source $DOTFILES/aliases/git.sh" >> ~/.bashrc
+
+# setup vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
--- vimrc
+++ vimrc
@@ -7,7 +7,7 @@ call vundle#begin()
 " let Vundle manage Vundle, required
 Plugin 'gmarik/Vundle.vim'
 
-for f in split(globpath('$DOTFILES/vim', '*.vim'), '\n')
+for f in split(globpath('$DOTFILES/vim/settings', '*.vim'), '\n')
   exe 'source' f
 endfor
 
