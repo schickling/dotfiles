92149bd0c67941b7e2c07fd4f40fbf243f13aefe
Johannes Schickling <schickling.j@gmail.com>
2014-06-29 22:08:03 +0200

    u

--- setup-minimal.sh
+++ setup-minimal.sh
@@ -4,6 +4,7 @@
 git clone --recursive https://github.com/schickling/dotfiles.git ~/.dotfiles
 
 # link vim
+mkdir ~/.vim
 ln -s ~/.dotfiles/vim/bundle ~/.vim/bundle
 ln -s ~/.dotfiles/vim/colors ~/.vim/colors
 ln -s ~/.dotfiles/vimrc ~/.vimrc
