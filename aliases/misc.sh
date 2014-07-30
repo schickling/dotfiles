5d6d275c159aacbce5383497a334dd07d09af629
Johannes Schickling <schickling.j@gmail.com>
2014-07-30 11:01:44 +0200

    fix docker

--- aliases/misc.sh
+++ aliases/misc.sh
@@ -27,7 +27,7 @@ alias cb="cordova build"
 
 # docker ##############
 alias d="docker"
-alias dcc="docker ps -q | xargs docker kill && docker ps -a -q | xargs docker rm"
+alias dcc="docker ps -q | xargs docker kill ; docker ps -aq | xargs docker rm"
 alias dci="dcc && docker images -q | xargs docker rmi"
 alias dir="docker run -i -t"
 alias b2d="boot2docker"
