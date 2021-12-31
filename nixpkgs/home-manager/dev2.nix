501fb87d6ba2839f0ec92e75141816fd579cc159
Johannes Schickling <schickling.j@gmail.com>
2021-12-31 13:25:51 +0100

    fix hm state version

--- nixpkgs/home-manager/dev2.nix
+++ nixpkgs/home-manager/dev2.nix
@@ -9,8 +9,8 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "21.05";
-  # home.stateVersion = "20.09";
+  # home.stateVersion = "21.05";
+  home.stateVersion = "20.09";
 
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
