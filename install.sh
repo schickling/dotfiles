1aff663250e2b3715087f222cc95f09e2198971d
Johannes Schickling <schickling.j@gmail.com>
2022-01-12 22:39:51 +0000

    gitpod

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
+set -e
+
 # install script for gitpod dotfiles support
 # https://gitpod.notion.site/Dotfiles-in-Gitpod-workspaces-b46b8723e9fe4efdbede72daa311961f
 
 mv ~/.config ~/.config-backup
 ln -sv $HOME/.dotfiles $HOME/.config
 mv ~/.config/fish ~/.config/fish-backup
-
 
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.11.tar.gz home-manager
 nix-channel --update
