9a78cc77bb8f01763700801b962260785dfd4c0f
AXON <axonasif@gmail.com>
2022-01-30 22:15:29 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,18 +14,19 @@ ln -sv $HOME/.dotfiles/nix $HOME/.config/nix
 mv ~/.config/nixpkgs ~/.config/nixpkgs-backup
 ln -sv $HOME/.dotfiles/nixpkgs $HOME/.config/nixpkgs
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.11.tar.gz home-manager
-nix-channel --update
+(
+  nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.11.tar.gz home-manager
+  nix-channel --update
+  nix-env -iA nixpkgs.nix
 
-nix-env -iA nixpkgs.nix
+  export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
 
-export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
+  nix-shell '<home-manager>' -A install
 
-nix-shell '<home-manager>' -A install
-
-pushd ~/.config/nixpkgs
-home-manager switch --flake .#gitpod
-popd
+  pushd ~/.config/nixpkgs
+  home-manager switch --flake .#gitpod
+  popd
+) & disown
 
 # use VSC settings.json from dotfiles
 mkdir -p $HOME/.config/Code/User
