a9eaaf07866d5c90458616a72a34d18e2879fb8e
Johannes Schickling <schickling.j@gmail.com>
2022-04-25 12:07:23 +0200

    tabs -> spaces

--- install.sh
+++ install.sh
@@ -9,48 +9,48 @@ date
 echo "Starting Gitpod install.sh..."
 
 if [[ -n "${GITPOD_DOTFILES_SKIP}" ]]; then
-	echo "Exiting Gitpod install.sh early..."
-	exit
+  echo "Exiting Gitpod install.sh early..."
+  exit
 fi
 
 setup_tailscale() {
-	# Run in background as it's blocking
-	sudo tailscaled &> ~/.tailscale.log &
+  # Run in background as it's blocking
+  sudo tailscaled &> ~/.tailscale.log &
 
-	time sudo -E tailscale up --hostname "gitpod-${GITPOD_WORKSPACE_ID}" --authkey "${TAILSCALE_AUTHKEY}"
+  time sudo -E tailscale up --hostname "gitpod-${GITPOD_WORKSPACE_ID}" --authkey "${TAILSCALE_AUTHKEY}"
 
-	date
+  date
 
-	# This is a special SSH key that allows my Gitpod instances to access my other Tailscale machines
-	mkdir -p ~/.ssh
-	# created via `cat ~/.ssh/id_rsa | base64 -w 0` on my main machine
-	echo "${SSHKEY_PRIVATE}" | base64 --decode > ~/.ssh/id_rsa
-	echo "${SSHKEY_PUBLIC}" | base64 --decode > ~/.ssh/id_rsa.pub
-	chmod 400 ~/.ssh/id_rsa
+  # This is a special SSH key that allows my Gitpod instances to access my other Tailscale machines
+  mkdir -p ~/.ssh
+  # created via `cat ~/.ssh/id_rsa | base64 -w 0` on my main machine
+  echo "${SSHKEY_PRIVATE}" | base64 --decode > ~/.ssh/id_rsa
+  echo "${SSHKEY_PUBLIC}" | base64 --decode > ~/.ssh/id_rsa.pub
+  chmod 400 ~/.ssh/id_rsa
 }
 
 main() {
-	setup_tailscale &
+  setup_tailscale &
 
-	# TODO this ideally lz4 should come pre-installed on Gitpod
-	sudo apt-get install lz4
+  # TODO this ideally lz4 should come pre-installed on Gitpod
+  sudo apt-get install lz4
 
-	date
+  date
 
-	NIX_RESULT_PATH=$(curl https://storage.googleapis.com/gitpod-test/result.closure.lz4 | lz4 -d  | nix-store --import | tail -1)
+  NIX_RESULT_PATH=$(curl https://storage.googleapis.com/gitpod-test/result.closure.lz4 | lz4 -d  | nix-store --import | tail -1)
 
-	date
+  date
 
-	$NIX_RESULT_PATH/activate
+  $NIX_RESULT_PATH/activate
 
-	# link VSC settings
+  # link VSC settings
 
-	# use VSC settings.json from dotfiles
-	mkdir -p $HOME/.config/Code/User
-	mv $HOME/.config/Code/User/settings.json $HOME/.config/Code/User/settings.json-backup
-	ln -s $HOME/.dotfiles/VSCode/settings.json $HOME/.config/Code/User/settings.json
+  # use VSC settings.json from dotfiles
+  mkdir -p $HOME/.config/Code/User
+  mv $HOME/.config/Code/User/settings.json $HOME/.config/Code/User/settings.json-backup
+  ln -s $HOME/.dotfiles/VSCode/settings.json $HOME/.config/Code/User/settings.json
 
-	date
+  date
 }
 
 time main
