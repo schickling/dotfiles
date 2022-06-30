c87a4bf1aa615926f313575f41deecac8e0fc44b
Johannes Schickling <schickling.j@gmail.com>
2022-06-30 12:07:45 +0200

    improved gitpod build script

--- build-gitpod-nix.sh
+++ build-gitpod-nix.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash -i
 
+# This script is ideally run on a Gitpod workspace.
+
 set -ex
 
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.11.tar.gz home-manager
@@ -19,9 +21,12 @@ pushd ~/.dotfiles/nixpkgs
 home-manager switch --flake .#gitpod
 popd
 
-# Requires `gcloud auth login` (e.g. via `nix shell nixpkgs#google-cloud-sdk`) before running
+# get path via `home-manager generations`
+nix-store --export (nix-store -qR /nix/store/mpkhk2l3036rb1516b2cfzbr4bb8gpag-home-manager-generation) | lz4 > result.closure.lz4
 
-nix shell nixpkgs#google-cloud-sdk
+# Requires `gcloud auth login` before running
+# (will prompt to run a similar command on machine with visual browser e.g. Mac)
+nix shell nixpkgs#google-cloud-sdk --command gcloud auth login
 
-gsutil cp result.closure.lz4 gs://schickling-gitpod-nix/result.closure.lz4
-gsutil acl ch -u AllUsers:R gs://schickling-gitpod-nix/result.closure.lz4
+nix shell nixpkgs#google-cloud-sdk --command gsutil cp result.closure.lz4 gs://schickling-gitpod-nix/result.closure.lz4
+nix shell nixpkgs#google-cloud-sdk --command gsutil acl ch -u AllUsers:R gs://schickling-gitpod-nix/result.closure.lz4
