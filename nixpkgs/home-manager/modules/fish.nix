922187f3e9db9036b47cae04fa80731180411269
Alif Arfab Pavel <aap.pavel@gmail.com>
2022-07-16 01:32:37 +0600

    feat(fish): added command `dr` to quick darwin build

--- nixpkgs/home-manager/modules/fish.nix
+++ nixpkgs/home-manager/modules/fish.nix
@@ -35,6 +35,12 @@
         end
       '';
 
+      dr = ''
+        pushd ~/.nixconfig
+        darwin-rebuild switch --flake .#$argv[1]
+        popd
+      '';
+
       hm = ''
         pushd ~/.config
         home-manager switch --flake .#$argv[1]
