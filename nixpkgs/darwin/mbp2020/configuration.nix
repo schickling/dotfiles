6a2d9d07f6747e3b303dda7fd249b6a09ee8cda1
Johannes Schickling <schickling.j@gmail.com>
2023-02-26 13:32:03 +0100

    1p

--- nixpkgs/darwin/mbp2020/configuration.nix
+++ nixpkgs/darwin/mbp2020/configuration.nix
@@ -55,6 +55,8 @@
     # NOTE we don't install `op` via nix but simply copy the binary
     mkdir -p /usr/local/bin
     cp ${pkgs._1password}/bin/op /usr/local/bin/op
+    # cp ${pkgs._1password}/bin/op-ssh-sign /usr/local/bin/op-ssh-sign
+    cp /Applications/1Password.app/Contents/MacOS/op-ssh-sign /usr/local/bin/op-ssh-sign
 
     # https://developer.1password.com/docs/ssh/get-started#step-4-configure-your-ssh-or-git-client
     mkdir -p ~/.1password && ln -sfv ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
