a19aede5e5fac3e8d69d776217eecca305e5b11e
Johannes Schickling <schickling.j@gmail.com>
2023-04-30 19:31:37 +0200

    gitx

--- nixpkgs/darwin/mbp2020/configuration.nix
+++ nixpkgs/darwin/mbp2020/configuration.nix
@@ -58,6 +58,9 @@
     # cp ${pkgs._1password}/bin/op-ssh-sign /usr/local/bin/op-ssh-sign
     cp /Applications/1Password.app/Contents/MacOS/op-ssh-sign /usr/local/bin/op-ssh-sign
 
+    # Make `gitx` available in the terminal
+    ln -sfv /Applications/GitX.app/Contents/Resources/gitx /usr/local/bin/gitx
+
     # https://developer.1password.com/docs/ssh/get-started#step-4-configure-your-ssh-or-git-client
     mkdir -p ~/.1password && ln -sfv ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
 
