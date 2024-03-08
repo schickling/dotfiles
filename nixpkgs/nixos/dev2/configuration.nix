fb5f5b147a3938093c5d5ef378bdf7dcd382f98c
Johannes Schickling <schickling.j@gmail.com>
2024-03-08 11:19:11 +0100

    upgrade

--- nixpkgs/nixos/dev2/configuration.nix
+++ nixpkgs/nixos/dev2/configuration.nix
@@ -52,7 +52,7 @@ in
   '';
   # The RAIDs are assembled in stage1, so we need to make the config
   # available there.
-  boot.initrd.services.swraid.mdadmConf = config.environment.etc."mdadm.conf".text;
+  # boot.initrd.services.swraid.mdadmConf = config.environment.etc."mdadm.conf".text;
 
   networking.hostName = "dev2";
   # Network (Hetzner uses static IP assignments, and we don't use DHCP here)
