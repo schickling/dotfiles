da91fffdf4900c3eeda3feb3a0560bb142646476
Johannes Schickling <schickling.j@gmail.com>
2025-07-11 10:14:50 +0200

    avoid enable-global-virtual-store

--- nixpkgs/home-manager/modules/pnpm.nix
+++ nixpkgs/home-manager/modules/pnpm.nix
@@ -4,6 +4,8 @@
   xdg.configFile."pnpm/rc".text = ''
     # Enable global virtual store for better disk space efficiency
     # This creates a single global store that all projects share
-    enable-global-virtual-store=true
+    # enable-global-virtual-store=true
+    # TODO re-enable when fixed github.com/pnpm/pnpm/issues/9739
+    enable-global-virtual-store=false
   '';
 }
